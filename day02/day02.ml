5e87dc68cc14b480b5a8bfd497f5cd017adc3ed7
francisco souza <fsouza@users.noreply.github.com>
2020-12-27 22:39:28 -0500

    day02 part2

--- day02/day02.ml
+++ day02/day02.ml
@@ -31,9 +31,26 @@ and run m pos operation =
   let target_addr = Mem.find (pos + 3) m in
   exec (Mem.add ~key:target_addr ~data:(operation x y) m) (pos + 4)
 
-let restore_gravity input =
-  input |> Mem.add ~key:1 ~data:12 |> Mem.add ~key:2 ~data:2
+let restore_gravity ?(noun = 12) ?(verb = 2) input =
+  input |> Mem.add ~key:1 ~data:noun |> Mem.add ~key:2 ~data:verb
 
 let part1 input = input |> restore_gravity |> Fun.flip exec 0 |> Mem.find 0
 
-let () = read_input () |> part1 |> Printf.printf "%d\n"
+let part2 target input =
+  let min_value = 0 in
+  let max_value = 99 in
+  let rec find_nound_and_verb noun verb =
+    let program_output =
+      input |> restore_gravity ~noun ~verb |> Fun.flip exec 0 |> Mem.find 0
+    in
+    if program_output = target then (noun, verb)
+    else if verb < max_value then find_nound_and_verb noun (verb + 1)
+    else if noun < max_value then find_nound_and_verb (noun + 1) min_value
+    else failwith "can't find the noun and verb!"
+  in
+  find_nound_and_verb 0 0 |> fun (noun, verb) -> (100 * noun) + verb
+
+let () =
+  let input = read_input () in
+  input |> part1 |> Printf.printf "Part 1: %d\n";
+  input |> part2 19690720 |> Printf.printf "Part 2: %d\n"
