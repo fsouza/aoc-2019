7491002cf7dcbcb63f6755bed04808b27805b5d1
francisco souza <fsouza@users.noreply.github.com>
2020-12-27 22:07:33 -0500

    day01 part2

--- day01/day01.ml
+++ day01/day01.ml
@@ -12,4 +12,22 @@ let read_input () =
 let part1 input =
   input |> List.map ~f:(fun n -> (n / 3) - 2) |> List.fold_left ~init:0 ~f:( + )
 
-let () = read_input () |> part1 |> Printf.printf "%d\n"
+let get_all_requirements n =
+  let rec get_all_requirements' acc n =
+    if n < 1 then acc
+    else
+      let new_n = (n / 3) - 2 in
+      get_all_requirements' (n :: acc) new_n
+  in
+  get_all_requirements' [] ((n / 3) - 2)
+
+let part2 input =
+  input
+  |> List.map ~f:get_all_requirements
+  |> List.flatten
+  |> List.fold_left ~init:0 ~f:( + )
+
+let () =
+  let input = read_input () in
+  Printf.printf "Part 1: %d\n" (input |> part1);
+  Printf.printf "Part 2: %d\n" (input |> part2)
